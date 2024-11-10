519c406d8cdb3fdbc1a0b9311ae9b868f7d4e88e
Halil24 <halilbagosi24@icloud.com>
2024-11-10 10:23:01 +0100

    Update loanApp.java

--- src/loanApp.java
+++ src/loanApp.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+public float pow(float a,float b) {
 
-public class loanApp {
+    public class loanApp {
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            double loanAmount;
+            int years;
+            float interest;
+
+            loanAmount = in.nextDouble();
+            years = in.nextInt();
+            interest = in.nextFloat();
+            int months = years * 12;
+            float monthlyInterest = interest / 100;
+            float k = Math.pow((1 + monthlyInterest), months);
+            float monthlyPayments = (float) loanAmount * ((monthlyInterest * k) / (k - 1));
+        }
+    }
 }
